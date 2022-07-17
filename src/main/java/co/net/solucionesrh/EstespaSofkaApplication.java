bf82984b7786d50baa88c23c51ece65d866fe0c9
Duhan Renteria <derh7280@hotmail.com>
2022-07-16 21:14:16 -0500

    Esquema inicial

---
+++ src/main/java/co/net/solucionesrh/EstespaSofkaApplication.java
@@ -0,0 +1,13 @@
+package co.net.solucionesrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstespaSofkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstespaSofkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/net/solucionesrh/EstespaSofkaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.net.solucionesrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstespaSofkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
