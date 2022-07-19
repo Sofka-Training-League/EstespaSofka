02f04e405b1e6f2f1e4a7ed204c2020fdd2e076a
Duhan Renteria <derh7280@hotmail.com>
2022-07-19 10:18:43 -0500

    Correccion bug busqueda

--- src/main/java/co/net/solucionesrh/repositorio/InterfaceTipos.java
+++ src/main/java/co/net/solucionesrh/repositorio/InterfaceTipos.java
@@ -3,8 +3,9 @@ package co.net.solucionesrh.repositorio;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,9 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 import co.net.solucionesrh.modelos.Tipos;
 
 @Repository
-public interface InterfaceTipos extends CrudRepository<Tipos, Integer>  {
+public interface InterfaceTipos extends JpaRepository<Tipos, Integer>  {
 
-    @Query(value="SELECT * FROM tbl_tipos_nave t WHERE LOWER(t.nombre) LIKE %nombre%")
+    @Query(nativeQuery = true, value="SELECT * FROM tbl_tipos_nave WHERE nombre LIKE %:nombre% ")
 	public List<Tipos> findByNombre(@Param("nombre") String nombre);
 
 
