f954668b64a14efd97d9b933bf4262fcf616136b
Duhan Renteria <derh7280@hotmail.com>
2022-07-19 20:54:54 -0500

    Busqueda, filtro y metodos de lanzamieento y aterrizaje

--- src/main/java/co/net/solucionesrh/controlador/NavesControlador.java
+++ src/main/java/co/net/solucionesrh/controlador/NavesControlador.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.net.solucionesrh.modelos.Estados;
 import co.net.solucionesrh.modelos.Naves;
+import co.net.solucionesrh.repositorio.InterfaceEstados;
 import co.net.solucionesrh.repositorio.InterfaceNaves;
 
 @RestController
@@ -26,15 +29,19 @@ public class NavesControlador {
 	@Autowired
 	private InterfaceNaves interfaceNaves;
 
+	@Autowired
+	private InterfaceEstados interfaceEstados;
+
 	// Listar todos los registros
 	@GetMapping
 	public List<Naves> naves() {
 		return (List<Naves>) interfaceNaves.findAll();
 	}
 
-	// Buscar registro por Id
+	// ----------------- Busquedas -----------------
+	// Buscar por Id
 	@GetMapping(value = "/{id}")
-	public Optional<Naves> buscarXId(@PathVariable("id") String strId) {
+	public Optional<Naves> buscarById(@PathVariable("id") String strId) {
 		try {
 			return interfaceNaves.findById(Long.parseLong(strId));
 		} catch (Exception e) {
@@ -43,7 +50,25 @@ public class NavesControlador {
 		}
 	}
 
-	// Insertar nuevo registro
+	//Buscar coincidencias en los nombres con like
+	@GetMapping(value = "/ByLike/{nombre}")
+	public List<Naves> likeNombre(@PathVariable("nombre") String nombre){
+		return interfaceNaves.findByLikeName(nombre);
+	}
+
+	//Buscar por nombre
+	@GetMapping(value = "/ByNombre/{nombre}")
+	public Optional<Naves> buscarByNombre(@PathVariable("nombre") String nombre){
+		return interfaceNaves.findByNombre(nombre);
+	}
+
+	//Buscar coincidencias en los nombres con like
+	@GetMapping(value = "/filtro/{filtro}")
+	public List<Naves> filterAdvanced(@PathVariable("filtro") String filtro){
+		return interfaceNaves.filterAdvanced(filtro);
+	}
+
+	// ----------------- Insertar nueva nave -----------------
 	@PostMapping
 	public Naves insertar(@RequestBody Naves nave) {
 		nave.setCodnave(null);
@@ -51,7 +76,7 @@ public class NavesControlador {
 		return interfaceNaves.save(nave);
 	}
 
-	// Actualizar registro
+	// ----------------- Actualizar nave -----------------
 	@PutMapping
 	public boolean modificar(@RequestBody Naves nave) {
 		try {
@@ -71,7 +96,7 @@ public class NavesControlador {
 		}
 	}
 
-	// Eliminar registro
+// ----------------- Eliminar registro -----------------
 	@DeleteMapping(value = "/{id}")
 	public boolean eliminar(@PathVariable("id") String strId) {
 		try {
@@ -83,9 +108,34 @@ public class NavesControlador {
 		}
 	}
 	
-	// Eliminar registro logicamente
-	@PutMapping(value = "/{id}")
-	public boolean eliminarLogicamente(@PathVariable("id") String strId) {
-		return true;
+	//Setear fecha de lanzamiento
+	@PatchMapping(value = "/iniciarmision/{id}")
+	public Optional<Naves> inicializandoExploracion(@RequestBody Naves nave, @PathVariable("id") Long id) {
+		Optional <Estados> myEstado =  interfaceEstados.findById(2);
+		Optional<Naves> myNave = interfaceNaves.findById(id);
+		if(!nave.getInexploracion().isEmpty() && myEstado.get().getCodigo()==2) {
+			myNave.get().setInexploracion(nave.getInexploracion());
+			myNave.get().setEstados(myEstado.get());
+			myNave.get().setEndexploracion(null);
+		}
+		interfaceNaves.save(myNave.get());
+		return myNave;
 	}
+
+	//Setear fecha de finalizacion lanzamiento
+	@PatchMapping(value = "/finalizarmision/{id}")
+	public Optional<Naves> finalizandoExploracion(@RequestBody Naves nave, @PathVariable("id") Long id) {
+		Optional <Estados> myEstado =  interfaceEstados.findById(3);
+		Optional<Naves> myNave = interfaceNaves.findById(id);
+		if(!nave.getEndexploracion().isEmpty() && myEstado.get().getCodigo()==3 ) {
+			if(myNave.get().getInexploracion()!=null)
+			{
+				myNave.get().setEndexploracion(nave.getEndexploracion());
+				myNave.get().setEstados(myEstado.get());
+			}
+		}
+		interfaceNaves.save(myNave.get());
+		return myNave;
+	}
+
 }
